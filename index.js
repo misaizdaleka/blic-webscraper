c546d5eb5e2c95ec21d08a377caea722e578a548
misaizdaleka <milos.boskovic@gmail.com>
2022-08-03 23:57:39 +0200

    Prvi setup - fetching stripova radi

--- index.js
+++ index.js
@@ -1,37 +1,58 @@
-const PORT = 8000
-const axios = require('axios')
-const cheerio = require('cheerio')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-app.use(cors())
+const PORT = 8000;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
 
-const url = 'https://www.theguardian.com/uk'
+const url = "https://www.blic.rs/blicstrip";
 
-app.get('/', function (req, res) {
-    res.json('This is my webscraper')
-})
+app.get("/", function (req, res) {
+  res.json("This is my webscraper");
+});
 
-app.get('/results', (req, res) => {
-    axios(url)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const articles = []
+app.get("/results", (req, res) => {
+  axios(url)
+    .then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const articles = [];
 
-            $('.fc-item__title', html).each(function () { //<-- cannot be a function expression
-                const title = $(this).text()
-                const url = $(this).find('a').attr('href')
-                articles.push({
-                    title,
-                    url
-                })
-            })
-            res.json(articles)
-        }).catch(err => console.log(err))
+      $("a:contains('Crta i piše')", html).each(function () {
+        //<-- cannot be a function expression
+        if (
+          !$(this).attr("href").includes("blic-strip") ||
+          $(this).attr("href").includes("strana=komentari") ||
+          articles.includes($(this).attr("href"))
+        ) {
+          return;
+        }
+        const url = $(this).attr("href");
+        articles.push(url);
+      });
+      const reqs = articles.slice(0, 50).map(a => axios(a));
+      //   console.log(reqs);
+      //   return axios.get(articles[0]);
+      return axios.all(reqs);
+    })
+    .then(responses => {
+      const imgs = [];
+      responses.forEach(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        $("div.img-wrapper img", html).each(function () {
+          $(this).attr("srcset") && imgs.push("http:" + $(this).attr("srcset"));
+        });
+      });
+      console.log(imgs);
+      const htmlImgs = imgs
+        .map(img => `<img src="${img}"/><br/><br/><br/>`)
+        .join("");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      return res.end(htmlImgs);
+    })
+    .catch(err => console.log(err));
+});
 
-})
-
-
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
-
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
