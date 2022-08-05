2526bcb0eadfaf5973df3557665e1833874e6735
misaizdaleka <milos.boskovic@gmail.com>
2022-08-05 23:49:07 +0200

    svašta nešto

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.use(cors());
 let collection;
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://itcatering:*******************************@cluster0.ur4pw.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongorue&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,108 +25,138 @@ app.get("/", function (req, res) {
   res.json("This is my webscraper");
 });
 
-app.get("/test", (req, res) => {
-  axios(url)
-    .then(response => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      return res.end(response.data);
-    })
-    .catch(err => console.log(err));
+app.get("/test", async (req, res) => {
+  const res2 = await client
+    .db("blicstrip")
+    .collection("strips")
+    .find()
+    .sort({ dateObj: -1 });
+  res2.toArray(function (err, result) {
+    result.forEach(r => {
+      console.log(r.date);
+    });
+  });
 });
 
 app.get("/results", (req, res) => {
-  const articles = [];
-  axios(url)
-    .then(async response => {
+  const saveImgsAndGenerateHtml = responses => {
+    const imgs = [];
+    responses.forEach(response => {
+      if (response.img) {
+        imgs.push(response.img);
+        return;
+      }
       const html = response.data;
       const $ = cheerio.load(html);
-      let count = 0;
-      let dbUpdates = [];
-      $("a:contains('Crta i piše')", html).each(function () {
-        //<-- cannot be a function expression
-        count++;
-        if (
-          (!$(this).attr("href").includes("blic-strip") &&
-            !$(this).attr("href").includes("crta-i-pise")) ||
-          $(this).attr("href").includes("strana=komentari") ||
-          articles.includes($(this).attr("href"))
-        ) {
-          console.error($(this).attr("href"));
+      $("div.img-wrapper img", html).each(function () {
+        if (!$(this).attr("srcset")) {
           return;
         }
-        const url = $(this).attr("href");
-        articles.push(url);
-        dbUpdates.push(
-          client
-            .db("blicstrip")
-            .collection("strips")
-            .updateOne(
-              {
-                adresa: url
-              },
-              {
-                $setOnInsert: { adresa: url, img: "" }
-              },
-              { upsert: true }
-            )
-        );
+        imgs.push("http:" + $(this).attr("srcset"));
+        client
+          .db("blicstrip")
+          .collection("strips")
+          .updateOne(
+            {
+              adresa: response.config.url
+            },
+            {
+              $set: { img: "http:" + $(this).attr("srcset") }
+            }
+          );
       });
-      console.log("Counted: ", count);
-      console.log("Counted filtered: ", articles.length);
-      return (
-        dbUpdates.length ? Promise.all(dbUpdates) : Promise.resolve(true)
-      ).then(() =>
-        Promise.all(
-          articles.slice(0, 50).map(a => {
-            return client
-              .db("blicstrip")
-              .collection("strips")
-              .findOne({ adresa: a })
-              .then(res => {
-                if (res.img) {
-                  return Promise.resolve({ img: res.img });
-                } else {
-                  return axios(a);
-                }
-              });
-          })
-        )
-      );
-    })
-    .then(responses => {
-      const imgs = [];
-      responses.forEach(response => {
-        if (response.img) {
-          imgs.push(response.img);
-          return;
-        }
+    });
+    const htmlImgs = imgs
+      .map(img => `<img src="${img}"/><br/><br/><br/>`)
+      .join("");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    return res.end(htmlImgs);
+  };
+
+  const fetchStrips = suffix => {
+    const articles = [];
+    return axios(url + suffix)
+      .then(response => {
         const html = response.data;
         const $ = cheerio.load(html);
-        $("div.img-wrapper img", html).each(function () {
-          if (!$(this).attr("srcset")) {
+        let dbUpdates = [];
+        $("article.news", html).each(function (e, el) {
+          const linkSearch = $(el).find("a:contains('Crta i piše')");
+
+          if (!linkSearch.length) {
             return;
           }
-          imgs.push("http:" + $(this).attr("srcset"));
-          client
-            .db("blicstrip")
-            .collection("strips")
-            .updateOne(
-              {
-                adresa: response.config.url
-              },
-              {
-                $set: { img: "http:" + $(this).attr("srcset") }
-              }
-            );
+
+          const date = $(el).find("time").text().trim();
+
+          const [day, month, year] = date.split(".");
+
+          const link = linkSearch.attr("href");
+
+          if (
+            (!link.includes("blic-strip") && !link.includes("crta-i-pise")) ||
+            link.includes("strana=komentari") ||
+            articles.includes(link)
+          ) {
+            console.error(link);
+            return;
+          }
+
+          const url = link;
+          articles.push(url);
+          dbUpdates.push(
+            client
+              .db("blicstrip")
+              .collection("strips")
+              .updateOne(
+                {
+                  adresa: url
+                },
+                {
+                  $setOnInsert: {
+                    adresa: url,
+                    date,
+                    dateObj: new Date(+year, +month - 1, +day),
+                    img: ""
+                  }
+                },
+                { upsert: true }
+              )
+          );
         });
-      });
-      const htmlImgs = imgs
-        .map(img => `<img src="${img}"/><br/><br/><br/>`)
-        .join("");
-      res.writeHead(200, { "Content-Type": "text/html" });
-      return res.end(htmlImgs);
-    })
-    .catch(err => console.log(err));
+        return dbUpdates.length
+          ? Promise.all(dbUpdates)
+          : Promise.resolve(false);
+      })
+      .then(res =>
+        articles.length
+          ? Promise.all(
+              articles.map(a =>
+                client
+                  .db("blicstrip")
+                  .collection("strips")
+                  .findOne({ adresa: a })
+                  .then(res2 => {
+                    if (res2 && res2.img) {
+                      return Promise.resolve({ img: res.img });
+                    } else {
+                      return axios(a);
+                    }
+                  })
+              )
+            )
+          : Promise.resolve([])
+      );
+  };
+
+  const fetch = (page = 0, responses = []) =>
+    fetchStrips(page ? "?strana=" + page : "").then(res =>
+      res.length
+        ? fetch(page + 1, [...responses, ...res])
+        : saveImgsAndGenerateHtml(responses)
+    );
+
+  // fetch();
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
